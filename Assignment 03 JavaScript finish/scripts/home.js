bbfc4313036ca0db49cdc2eb07337250e811491c
Phan Thị Lan Anh <103052546+PhanLanAnh0609@users.noreply.github.com>
2023-08-22 14:35:47 +0700

    Add files via upload

---
+++ Assignment 03 JavaScript finish/scripts/home.js
@@ -0,0 +1,25 @@
+'use strict'
+const loginModal = document.getElementById('login-modal');
+const mainContent = document.getElementById('main-content');
+const welcomeMessage = document.getElementById('welcome-message');
+
+// khai báo nút logout
+const btnLogout = document.getElementById('btn-logout');
+
+// thay đổi giao diện trang home
+if(!currentUser) {
+    // ẩn giao diện đăng nhập thành công
+    mainContent.style.display = 'none';
+} else {
+    // ẩn giao diện trước khi đăng nhập
+    loginModal.style.display = 'none';
+    // hiển thị lời chào sau khi đăng nhập thành công
+    welcomeMessage.textContent = `Welcom ${currentUser.firstName}`
+}
+
+
+// Tạo sự kiện diễn ra khi nhấn nút Logout
+btnLogout.addEventListener('click', function() {
+    localStorage.removeItem('currentUser');
+    window.location.href = '../pages/login.html';
+})
